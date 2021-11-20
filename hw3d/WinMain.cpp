eeea8cb9ac49f3a94b48196fa3f8ef74df833048
Jay <jaykhandelwal22@gmail.com>
2021-11-20 17:36:53 +0530

    WIndow Framework

---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,26 @@
+#include"Window.h"
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	Window wnd(800, 300, "Win Title");
+
+
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+
+	return msg.wParam;
+}
